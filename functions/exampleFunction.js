6ccab60b356ad7c432e625fc6cd1c8729c63bcbc
Tanner Rigby <tanner.rigby@canopytax.com>
2019-06-28 10:18:10 -0600

    first commit

---
+++ functions/exampleFunction.js
@@ -0,0 +1,50 @@
+import { failure, serverFailure, success } from '../utils/response';
+import { isLambdaWarmer } from '../utils/warmer';
+
+export async function main(event) {
+  return new Promise(async (resolve, reject) => {
+    if (isLambdaWarmer(event)) return resolve(success());
+
+    const data = event.queryStringParameters;
+
+    console.info('Event Received: ', data);
+
+    // check to make sure the data array has values
+    if (!data) {
+      console.error('No data to work with!');
+      return resolve({
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify({ error: 'no data to work with' })
+      });
+    }
+
+    try {
+      const splitString = data.word.split("")
+      const reverseArray = splitString.reverse()
+      const backwardString = reverseArray.join("");
+
+      return resolve({
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify({ backwardString })
+      });
+    } catch (error) {
+      console.error('probably not a valid string');
+      return resolve({
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify({ error })
+      });
+    }
+  });
+}
---
+++ models/exampleModel.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+
+const user = {
+    userId: '',
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    createdAt: '',
+    updatedAt: '',
+  };
+
+  function getUserKeys() {
+    return Object.keys(user);
+  }
+  
+  export function getUserKeysWithoutSecrets() {
+    const userKeys = getUserKeys();
+  
+    return _.remove(userKeys, value => {
+      return (
+        value !== 'password'
+      );
+    });
+  }
+  
+  export function getUser(data) {
+    const userKeys = getUserKeys();
+    const prunedData = _.cloneDeep(data);
+  
+    _.forIn(data, (value, key) => {
+      // check if key exists on User object
+      const index = _.indexOf(userKeys, key);
+  
+      if (index < 0) {
+        // It doesn't exist, don't allow it to be added to db
+        delete prunedData[key];
+      } else if (!value) {
+        // No value, delete it
+        delete prunedData[key];
+      }
+    });
+  
+    return prunedData;
+  }
